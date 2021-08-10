a1aab08674f8c95907cf56384a3b91a768b97fe8
Ricky Zaman <ricky.zaman@outlook.com>
2021-08-09 22:51:39 -0400

    created headerItem component

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,28 @@
 import Image from "next/image";
+import HeaderItem from '../components/HeaderItem'; 
+import { BadgeCheckIcon,
+        CollectionIcon,
+        HomeIcon,
+        LightningBoltIcon,
+        SearchIcon,
+        UserIcon
+} from '@heroicons/react/outline'
 
 const Header = () => {
     return (
         <header>
-            <h1>Hulu 2.0</h1>   
-            {/* Icons */}
-            <Image src="https://links.papareact.com/ua6"
-            width={200} height={100}
-            />
-            {/* hulu logo */}
+            <div>
+                <HeaderItem title="Home" Icon={HomeIcon}/>
+
+                {/* Icons */}
+            </div>
+            <div>
+                <Image src="https://links.papareact.com/ua6"
+                    className="object-contain"
+                    width={200} height={100}
+                />
+            </div>
+
         </header>
     )
 }
